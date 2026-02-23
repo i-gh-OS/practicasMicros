d5f4eccb4fa70626f4f19101b98f5751f26c3668
i-gh-OS <202301839@alu.comillas.edu>
2026-02-23 11:33:14 +0100

    Create timer2.c

---
+++ Pract3/Pract3.X/timer2.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   timer2.c
+ * Author: Irene y Guille
+ *
+ * Created on 23 de febrero de 2026, 10:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
