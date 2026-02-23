0e3244853124a4f9d18d00dc3628fb95f010cc9e
i-gh-OS <202301839@alu.comillas.edu>
2026-02-23 10:57:13 +0100

    añadido apagar LED a los 4s

--- Pract4/Pract4.X/main3.c
+++ Pract4/Pract4.X/main3.c
@@ -11,19 +11,20 @@
 #define PIN_PULSADOR 5
 #define LED_RC1 1
 #define LED_RC0 0
-#define N_MAX 5;
+#define N_MAX 5
 
 static int pulsac = 0;
-//int segundos=0;
+static int tienes_contar=0;
+static int segundos=0;
 
 __attribute__((vector(_TIMER_2_VECTOR), interrupt(IPL2SOFT), nomips16))
 void InterrupcionTimer2(void) {
-    
     LATCINV = (1 << LED_RC0);
     pulsac = 0;
     IFS0bits.T2IF = 0;
-    if tienes_
-            segundos++;
+    if (tienes_contar){
+        segundos++;
+    }
 }
 
 int main(void) {
@@ -52,24 +53,24 @@ int main(void) {
     puls_ant = (PORTB >> PIN_PULSADOR) & 1;
     while (1) {
         puls_act = (PORTB >> PIN_PULSADOR) & 1;
-        //while(segundos <= 4){
-
         if ((puls_ant != puls_act) && (puls_act == 0)) {
             asm("di");
-            pulsac=pulsac+1;
-            if (pulsac >= 5) {
-                LATCCLR = 1 << LED_RC1;
-                tienes_contar=1;
+            pulsac++;
+            if (pulsac >= N_MAX) {
+                LATCCLR = 1 << LED_RC1; //enciende LED
+                tienes_contar = 1;
             }
-            asm("ei");
-            
+            asm("ei");  
         }
-
-        //}
-        //segundos = 0;
         puls_ant = puls_act;
+
+        if (segundos >= 4) {
+            asm("di");
+            LATCSET = 1 << LED_RC1; //apago LED
+            tienes_contar = 0;
+            segundos = 0;
+            asm("ei");
+        }
     }
-
-
     return 0;
 }
