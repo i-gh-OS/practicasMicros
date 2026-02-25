246d0173695b8f8b9d5ba5cc4444821d8b61b5ae
i-gh-OS <202301839@alu.comillas.edu>
2026-02-25 08:23:09 +0100

    act prcat5

---
+++ Pract5/Pract5.X/main1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: irene
+ *
+ * Created on February 24, 2026, 7:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
